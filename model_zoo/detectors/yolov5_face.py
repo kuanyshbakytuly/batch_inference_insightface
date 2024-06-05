0a2823904a7775ad53f0fd7eb691a1da070e765d
kuanysbakytuly <topppherooo@gmail.com>
2024-06-05 19:34:38 +0500

    a change

--- model_zoo/detectors/yolov5_face.py
+++ model_zoo/detectors/yolov5_face.py
@@ -7,13 +7,13 @@ import cv2
 import numpy as np
 from numba import njit
 
-from model_zoo.detectors.common.nms import nms
-from model_zoo.exec_backends.onnxrt_backend import DetectorInfer as DIO
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.common.nms import nms
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.exec_backends.onnxrt_backend import DetectorInfer as DIO
 
 # Since TensorRT and pycuda are optional dependencies it might be not available
 try:
     import cupy as cp
-    from model_zoo.exec_backends.trt_backend import DetectorInfer as DIT
+    from src.cv.face_recognition.batch_inference_insightface.model_zoo.exec_backends.trt_backend import DetectorInfer as DIT
 except BaseException:
     DIT = None
 
--- model_zoo/face_detectors.py
+++ model_zoo/face_detectors.py
@@ -1,8 +1,8 @@
-from model_zoo.detectors.retinaface import RetinaFace
-from model_zoo.detectors.centerface import CenterFace
-from model_zoo.detectors.dbface import DBFace
-from model_zoo.detectors.scrfd import SCRFD
-from model_zoo.detectors.yolov5_face import YoloV5
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.retinaface import RetinaFace
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.centerface import CenterFace
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.dbface import DBFace
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.scrfd import SCRFD
+from src.cv.face_recognition.batch_inference_insightface.model_zoo.detectors.yolov5_face import YoloV5
 
 
 def get_retinaface(model_path, backend, outputs, rac, masks=False, **kwargs):
